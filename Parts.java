9f86fea5f25fa919fbbd4a46636e9d870eef59cd
matsumura-himejo <matsumura@himejo.jp>
2023-11-14 14:22:24 +0900

    プッシュ２

--- Parts.java
+++ Parts.java
@@ -2,6 +2,6 @@ public class Parts {
     public static void show(){
         System.out.println("Parts.show()");
         System.out.println("新たな変更を加えた");
-        System.out.println("更に新たな変更を加えた");
+        System.out.println("更に新たな変更加えた");
     }
 }
