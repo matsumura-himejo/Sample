a5c439ccea390db341a2bfb2ff589a99569ef200
matsumura-himejo <matsumura@himejo.jp>
2023-11-14 13:40:45 +0900

    2ファイル更新

---
+++ Parts.java
@@ -0,0 +1,7 @@
+public class Parts {
+    public static void show(){
+        System.out.println("Parts.show()");
+        System.out.println("新たな変更を加えた");
+        System.out.println("更に新たな変更を加えた");
+    }
+}
---
+++ Sample.java
@@ -0,0 +1,7 @@
+class Sample{
+    public static void main(String[] args) {
+        System.out.println("テストここも変更");
+        Parts.show();
+        System.out.println("更新後！");
+    }
+}
